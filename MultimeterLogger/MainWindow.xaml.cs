d8de5a302f084b84a2c544cf4a56bda6e9a6f58f
godFather89 <tatarandrei@gmail.com>
2014-02-04 17:04:07 +0200

    as of now

---
+++ MultimeterLogger/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultimeterLogger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private readonly MainWindowViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = _viewModel = new MainWindowViewModel();
+        }
+    }
+}
